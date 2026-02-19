517eab33c2617ba8c83ede5958ac707db4683132
JakeBSIT3101 <22-78222@g.batstate-u.edu.ph>
2026-02-19 20:10:09 +0800

    Archive modal

--- src/app/layout/component/modal/modal.ts
+++ src/app/layout/component/modal/modal.ts
@@ -1,11 +1,12 @@
 import { Component, EventEmitter, Output } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-modal',
   standalone: true,
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, RouterModule],
   templateUrl: './modal.html',
   styleUrls: ['./modal.scss'],
 })
@@ -18,6 +19,8 @@ export class Modal {
     { name: 'Account Access Inquiry', date: 'February 16, 2026', checked: false },
   ];
 
+  constructor(private router: Router) {}
+
   onClose() {
     this.close.emit();
   }
@@ -28,5 +31,9 @@ export class Modal {
 
   toggleAll(checked: boolean) {
     this.rows = this.rows.map(r => ({ ...r, checked }));
+  }
+
+  goToPreview() {
+    this.router.navigate(['/preview-archive']);
   }
 }
